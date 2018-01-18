d82008ed88324c58b45ec94b96d3f50d9fac19c2
Tara Ellicott <taraellicott@gmail.com>
2018-01-18 21:53:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 const app = "I don't do much."
+
+
+kittens = ["Milo", "Otis", "Garfield"]; 
+
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+
+function appendKitten(name) {
+  
+}
+
+function prependKitten(name) {
+  
+}
+
+function removeLastKitten() {
+  kittens.slice(0, -1);
+}
+
+function removeFirstKitten() {
+  kittens.slice(1,kittens.length-1);
+  return kittens;
+}
